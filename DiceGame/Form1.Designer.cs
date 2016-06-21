d3a99f6670c5a507d013333e41eb19b39ce8611a
Filiberto Rios <rios.filiberto.98@gmail.com>
2016-06-21 11:47:10 -0500

    First Commit

---
+++ DiceGame/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace DiceGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.bet = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Location = new System.Drawing.Point(182, 219);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(83, 23);
+            this.btnRoll.TabIndex = 0;
+            this.btnRoll.Text = "Bet";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "$100";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(202, 113);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(29, 13);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "Dice";
+            this.lblMessage.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // bet
+            // 
+            this.bet.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.bet.Location = new System.Drawing.Point(3, 222);
+            this.bet.Maximum = new decimal(new int[] {
+            276447231,
+            23283,
+            0,
+            0});
+            this.bet.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.bet.Name = "bet";
+            this.bet.Size = new System.Drawing.Size(88, 20);
+            this.bet.TabIndex = 4;
+            this.bet.ThousandsSeparator = true;
+            this.bet.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.bet.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "$ Available";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 24);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 306);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bet);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRoll);
+            this.Name = "Form1";
+            this.Text = "Roll-a-4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.NumericUpDown bet;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DiceGame/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiceGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            lblMessage.Text = dice.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
